6996dfd68d0ecac57bc136a7e0dfbd0963200255
abhishekdhull <46604435+abhishekdhull21@users.noreply.github.com>
2025-05-03 21:22:54 +0530

    htaccess changed

--- api/application/config/rest.php
+++ api/application/config/rest.php
@@ -601,4 +601,9 @@ $config['allow_any_cors_domain'] = FALSE;
   | e.g. $config['allowed_origins'] = ['http://www.example.com', 'https://spa.example.com']
   |
  */
-$config['allowed_cors_origins'] = [];
+$config['allowed_cors_origins'] = [
+  'https://paisaonsalary.in',
+  'https://*.paisaonsalary.in',  // Allow all subdomains
+  'https://localhost',
+  'http://localhost',
+];
