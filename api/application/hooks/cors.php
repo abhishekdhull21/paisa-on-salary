2314f9fa38bab03d6a00f67f6a469bd520f69eed
abhishekdhull <46604435+abhishekdhull21@users.noreply.github.com>
2025-05-03 23:07:57 +0530

    htaccess changed

--- api/application/hooks/cors.php
+++ api/application/hooks/cors.php
@@ -3,11 +3,12 @@
 function add_cors_headers() {
     header("Access-Control-Allow-Origin: *"); // Allow all origins, or restrict to specific domain
     header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE"); // Allowed HTTP methods
-    header("Access-Control-Allow-Headers: Content-Type, Authorization, Auth"); // Allow custom headers, including 'auth'
+    header("Access-Control-Allow-Headers: Content-Type, Authorization, Auth, X-Requested-With"); // Allow custom headers, including 'auth'
 
     // Handle OPTIONS request
     if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
-        header('HTTP/1.1 200 OK');
+        // header('HTTP/1.1 200 OK');
+        http_response_code(200);
         exit();
     }
 }
